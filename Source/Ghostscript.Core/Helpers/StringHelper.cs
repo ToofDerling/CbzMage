4f86ebe371004ba66b2a3e30d087c11807f42675
maxgit <rotvel@gmail.com>
2022-10-16 03:36:30 +0200

    Bring in Ghostscript.Core

---
+++ Source/Ghostscript.Core/Helpers/StringHelper.cs
@@ -0,0 +1,67 @@
+﻿//
+// StreamHelper.cs
+// This file is part of Ghostscript.NET library
+//
+// Author: Josip Habjan (habjan@gmail.com, http://www.linkedin.com/in/habjan) 
+// Copyright (c) 2013-2021 by Josip Habjan. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ghostscript.NET
+{
+    public class StringHelper
+    {
+        #region ToUtf8String
+
+        public static string ToUtf8String(string value)
+        {
+            return Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(value)));
+        }
+
+        #endregion
+
+        #region NativeUtf8FromString
+
+        public static IntPtr NativeUtf8FromString(string managedString)
+        {
+            int len = Encoding.UTF8.GetByteCount(managedString);
+            byte[] buffer = new byte[len + 1]; // null-terminator allocated
+            Encoding.UTF8.GetBytes(managedString, 0, managedString.Length, buffer, 0);
+            IntPtr nativeUtf8 = Marshal.AllocHGlobal(buffer.Length);
+            Marshal.Copy(buffer, 0, nativeUtf8, buffer.Length);
+            return nativeUtf8;
+        }
+
+        #endregion
+
+        #region HasNonASCIIChars
+
+        public static bool HasNonASCIIChars(string str)
+        {
+            return (Encoding.UTF8.GetByteCount(str) != str.Length);
+        }
+
+        #endregion
+    }
+}
