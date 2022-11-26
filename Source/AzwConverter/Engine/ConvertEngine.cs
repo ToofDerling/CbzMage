b691616b9425f7f91b2414c82051ddf2fc41a69c
maxgit <rotvel@gmail.com>
2022-11-26 04:04:12 +0100

    Cleanup

--- Source/AzwConverter/Engine/ConvertEngine.cs
+++ Source/AzwConverter/Engine/ConvertEngine.cs
@@ -47,25 +47,6 @@ namespace AzwConverter.Engine
             return state;
         }
 
-        /*
-
-        using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
-   {
-      var demoFile = archive.CreateEntry("foo.txt");
-
-      using (var entryStream = demoFile.Open())
-      using (var streamWriter = new StreamWriter(entryStream))
-      {
-         streamWriter.Write("Bar!");
-      }
-   }
-
-   using (var fileStream = new FileStream(@"C:\Temp\test.zip", FileMode.Create))
-   {
-      memoryStream.Seek(0, SeekOrigin.Begin);
-      memoryStream.CopyTo(fileStream);
-   }
-        */
         private CbzState ReadAndCompress(string tempFile, PageRecords? hdImageRecords, PageRecords sdImageRecords)
         {
             CbzState state;
