771c8f829acee2ad1ab9a81a09c2bba978d17e7b
maxgit <rotvel@gmail.com>
2022-12-08 21:57:43 +0100

    Tune thread calculation

--- Source/CbzMage.Shared/Helpers/SettingsHelper.cs
+++ Source/CbzMage.Shared/Helpers/SettingsHelper.cs
@@ -24,13 +24,17 @@ namespace CbzMage.Shared.Helpers
 
         public int GetThreadCount(int settingsThreadCount)
         {
+            const double fraction = 0.75;
+
             const int maxThreads = 8;
             const int minThreads = 2;
 
             if (settingsThreadCount <= 0)
             {
-                var calculatedThreadCount = (Environment.ProcessorCount / 2) + 1;
+                var threadCountFraction = Environment.ProcessorCount / fraction;
                 
+                var calculatedThreadCount = Convert.ToInt32(threadCountFraction);
+
                 calculatedThreadCount = Math.Min(calculatedThreadCount, maxThreads);
                 calculatedThreadCount = Math.Max(calculatedThreadCount, minThreads);
 
