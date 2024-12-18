583d06d88bdfff3d65ee9386f2f7cf9d8bc35664
Toof <Dont@Me>
2024-12-18 16:51:49 +0100

    Float & double to int

---
+++ Source/CbzMage.Shared/Extensions/NumberExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CbzMage.Shared.Extensions
+{
+    public static class NumberExtensions
+    {
+        public static int ToInt(this float f) => Convert.ToInt32(f);
+
+        public static int ToInt(this double d) => Convert.ToInt32(d);
+    }
+}
